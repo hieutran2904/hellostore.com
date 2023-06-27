08dc52ceee06c9b39cb606fa54e2c56def2aa1ed
hieutran <hieu80564@nuce.edu.vn>
2023-06-27 21:25:49 +0700

    check session login admin

--- admin/manage-product-detail.php
+++ admin/manage-product-detail.php
@@ -1,6 +1,10 @@
 <?php
 include("app/Controllers/View.php");
 $view = new View;
+if(!isset($_SESSION['admin_id'])) {
+    header("Location: index.php");
+    exit();
+}
 $view->loadContent("include", "top");
 $view->loadContent("include", "sidebar");
 $view->loadContent("content", "add-edit-product-detail");
