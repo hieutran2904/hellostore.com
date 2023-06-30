f56267a75b4e4da48ba1a98e6410d87776726d23
hieutran <hieu80564@nuce.edu.vn>
2023-06-30 10:27:25 +0700

    fix show product sold, remain

--- admin/app/Controllers/ProductRemainController.php
+++ admin/app/Controllers/ProductRemainController.php
@@ -3,7 +3,7 @@ class ProductRemainController extends Controller
 {
     public function ProductList($productList)
     {
-        $i=0;   
+        $i = 0;
         foreach ($productList as $eachProduct) {
             $imageProduct = $eachProduct['product_master_image'] == '' ? $GLOBALS['NO_IMAGE'] : $GLOBALS['PRODUCT_DIRECTORY'] . $eachProduct['product_master_image'];
             $productRemain = $eachProduct['quantity_remain'] == '' ? 0 : $eachProduct['quantity_remain'];
@@ -14,8 +14,8 @@ class ProductRemainController extends Controller
                 <td><a href="product-detail.php?id=<?= $eachProduct['productId'] ?>">
                         <?= $eachProduct['product_name'] ?>
                     </a></td>
-                <td><?= number_format($eachProduct['productPrice'], 0, ',', '.') . $GLOBALS['CURRENCY'] ?></td>
                 <td><?= $eachProduct['subcategory_name'] ?></td>
+                <td><?= number_format($eachProduct['productPrice'], 0, ',', '.') . $GLOBALS['CURRENCY'] ?></td>
                 <td>
                     <span class="text-warning">
                         <?= $productRemain ?>
